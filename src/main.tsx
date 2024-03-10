5ed9ba7fc0ef14ee4b21bdfc7401cbc698170828
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 12:23:19 +0100

    Updates main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import { DataProvider } from "./context/DataProvider.tsx"
+import { FontProvider } from "./context/FontProvider.tsx"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <FontProvider>
+            <DataProvider>
+                <App />
+            </DataProvider>
+        </FontProvider>
+    </React.StrictMode>,
 )
