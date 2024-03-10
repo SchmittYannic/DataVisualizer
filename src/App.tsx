bd6faff31dbb1dfd9aa9402578bc9671845186a9
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 19:02:53 +0100

    Updates App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Homepage = lazy(() => import("./components/Homepage" /* webpackChunkName: "Homepage" */));
+const UploadStep = lazy(() => import("./features/datavisualizer/UploadStep" /* webpackChunkName: "UploadStep" */));
+const DataStep = lazy(() => import("./features/datavisualizer/DataStep" /* webpackChunkName: "DataStep" */));
+const VisualizationStep = lazy(() => import("./features/datavisualizer/VisualizationStep" /* webpackChunkName: "VisualizationStep" */));
+const ProgressSteps = lazy(() => import("./components/ProgressSteps" /* webpackChunkName: "ProgressStep" */));
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export const steps = [
+    {
+        stepcount: 1,
+        label: "Upload",
+        component: UploadStep,
+        path: "/DataVisualizer/UploadStep",
+    },
+    {
+        stepcount: 2,
+        label: "Datenansicht",
+        component: DataStep,
+        path: "/DataVisualizer/DataStep",
+    },
+    {
+        stepcount: 3,
+        label: "Visualisierung",
+        component: VisualizationStep,
+        path: "/DataVisualizer/VisualizationStep",
+    },
+]
+
+const App = () => {
+    return (
+        <Router>
+            <ScrollToTop>
+                <Routes>
+                    <Route path="/*" element={<Suspense><Homepage /></Suspense>}/>
+                    {steps.map((step, idx) => (
+                        <Route key={idx} path={step.path} element={
+                            <Suspense>
+                                <ProgressSteps />
+                                <step.component />
+                            </Suspense>
+                        }/>
+                    ))}
+                </Routes>
+            </ScrollToTop>
+        </Router>
+    )
 }
 
-export default App
+export default App;
