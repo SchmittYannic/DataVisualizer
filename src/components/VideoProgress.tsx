f174d9c63a7f1684de77322d9ecd6e8e1d11fdb6
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 15:22:20 +0100

    Adds VideoProgress component

---
+++ src/components/VideoProgress.tsx
@@ -0,0 +1,77 @@
+import { AnimationPlaybackControls, useAnimate } from "framer-motion";
+import { useEffect, useState } from "react";
+
+import useIsDocumentHidden from "../hooks/useIsDocumentHidden";
+
+type VideoProgressPropsType = {
+    videoRef: React.RefObject<HTMLVideoElement>,
+    vertical?: boolean,
+}
+
+const VideoProgress = ({ videoRef, vertical=true }: VideoProgressPropsType) => {
+
+    const [scope, animate] = useAnimate();
+    const isDocumentHidden = useIsDocumentHidden();
+    const [duration, setDuration] = useState(0);
+    const [animation, setAnimation] = useState<AnimationPlaybackControls>();
+
+    useEffect(() => {
+        if (!videoRef.current) return
+
+        const video = videoRef.current;
+
+        const videoLoading = new Promise<void>(resolve => {
+            const handleEvent = () => {
+                video.removeEventListener("loadedmetadata", handleEvent);
+                resolve();
+            }
+
+            video.addEventListener("loadedmetadata", handleEvent);
+        })
+
+        videoLoading.then(() => {
+            setDuration(video.duration);
+        });
+    }, [videoRef]);
+
+    useEffect(() => {
+        if (!scope.current) return
+
+        const keyframes = vertical ? { height: ["100%", "0%"] } : { width: ["100%", "0%"] };
+
+        const animation = animate(
+            scope.current,
+            keyframes,
+            { duration: duration, ease: "linear" }
+        );
+
+        setAnimation(animation);
+    }, [duration, animate, scope, vertical]);
+
+    useEffect(() => {
+        if (!animation) return
+        if (!isDocumentHidden) {
+            animation.play();
+        } else {
+            animation.pause();
+        }
+    }, [isDocumentHidden, animation]);
+
+    return (
+        <>
+            {(duration > 0) && (
+                <div 
+                    className="progressbar"
+                >    
+                    <div
+                        ref={scope}
+                        className="progressbar-done"
+                    >
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default VideoProgress
