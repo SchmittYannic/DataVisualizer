4d7df6c83586a3e11d288f2605790f299143be68
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 16:31:12 +0100

    Adds DebouncedInput ui component

---
+++ src/components/ui/DebouncedInput.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, ReactElement, KeyboardEvent } from "react";
+
+type DebouncedInputType = {
+    value: string | number
+    onChange: (value: string | number) => void
+    debounce?: number
+}
+
+const DebouncedInput = ({
+    value: initialValue,
+    onChange,
+    debounce = 500,
+    ...props
+}: DebouncedInputType & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">): ReactElement => {
+    const [value, setValue] = useState(initialValue);
+
+    const onInputKeydown = (e: KeyboardEvent<HTMLInputElement>) => {
+        const { key } = e;
+        // Only allow number and utility keys to be functional.
+        if (!/[0-9]/.test(key) && !["Backspace", "Delete", "Tab", "ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight", "Enter"].includes(key)) {
+            e.preventDefault();
+        }
+    };
+  
+    useEffect(() => {
+        setValue(initialValue);
+    }, [initialValue]);
+  
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            onChange(value);
+        }, debounce);
+    
+        return () => clearTimeout(timeout)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value]);
+  
+    return (
+        <input
+            {...props}
+            value={value}
+            onChange={e => setValue(e.target.value)}
+            onKeyDown={props.type === "number" ? onInputKeydown : () => {}}
+            inputMode={props.type === "number" ? "numeric" : "text"}
+        />
+    )
+}
+
+export default DebouncedInput
