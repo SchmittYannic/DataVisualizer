1e30e0f16cd75ba63ea86ce6960a849d31bd1e1f
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 12:23:09 +0100

    Adds FontProvider

---
+++ src/context/FontProvider.tsx
@@ -0,0 +1,46 @@
+import { PropsWithChildren, createContext, useEffect, useState } from "react";
+
+export type FontContextType = {
+    availableFonts: string[],
+}
+
+const initContextState = {
+    availableFonts: [],
+};
+
+export const FontContext = createContext<FontContextType>(initContextState);
+
+export const FontProvider = ({ children }: PropsWithChildren) => {
+    const [availableFonts, setAvailableFonts] = useState<string[]>([])
+
+    useEffect(() => {
+        const fontCheck = new Set([
+            // Windows 10
+            'Arial', 'Arial Black', 'Bahnschrift', 'Calibri', 'Cambria', 'Cambria Math', 'Candara', 'Comic Sans MS', 'Consolas', 'Constantia', 'Corbel', 'Courier New', 'Ebrima', 'Franklin Gothic Medium', 'Gabriola', 'Gadugi', 'Georgia', 'HoloLens MDL2 Assets', 'Impact', 'Ink Free', 'Javanese Text', 'Leelawadee UI', 'Lucida Console', 'Lucida Sans Unicode', 'Malgun Gothic', 'Marlett', 'Microsoft Himalaya', 'Microsoft JhengHei', 'Microsoft New Tai Lue', 'Microsoft PhagsPa', 'Microsoft Sans Serif', 'Microsoft Tai Le', 'Microsoft YaHei', 'Microsoft Yi Baiti', 'MingLiU-ExtB', 'Mongolian Baiti', 'MS Gothic', 'MV Boli', 'Myanmar Text', 'Nirmala UI', 'Palatino Linotype', 'Segoe MDL2 Assets', 'Segoe Print', 'Segoe Script', 'Segoe UI', 'Segoe UI Historic', 'Segoe UI Emoji', 'Segoe UI Symbol', 'SimSun', 'Sitka', 'Sylfaen', 'Symbol', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana', 'Webdings', 'Wingdings', 'Yu Gothic',
+            // macOS
+            'American Typewriter', 'Andale Mono', 'Arial', 'Arial Black', 'Arial Narrow', 'Arial Rounded MT Bold', 'Arial Unicode MS', 'Avenir', 'Avenir Next', 'Avenir Next Condensed', 'Baskerville', 'Big Caslon', 'Bodoni 72', 'Bodoni 72 Oldstyle', 'Bodoni 72 Smallcaps', 'Bradley Hand', 'Brush Script MT', 'Chalkboard', 'Chalkboard SE', 'Chalkduster', 'Charter', 'Cochin', 'Comic Sans MS', 'Copperplate', 'Courier', 'Courier New', 'Didot', 'DIN Alternate', 'DIN Condensed', 'Futura', 'Geneva', 'Georgia', 'Gill Sans', 'Helvetica', 'Helvetica Neue', 'Herculanum', 'Hoefler Text', 'Impact', 'Lucida Grande', 'Luminari', 'Marker Felt', 'Menlo', 'Microsoft Sans Serif', 'Monaco', 'Noteworthy', 'Optima', 'Palatino', 'Papyrus', 'Phosphate', 'Rockwell', 'Savoye LET', 'SignPainter', 'Skia', 'Snell Roundhand', 'Tahoma', 'Times', 'Times New Roman', 'Trattatello', 'Trebuchet MS', 'Verdana', 'Zapfino',
+        ].sort());
+    
+        const getAvailableFonts = async () => {
+            await document.fonts.ready;
+    
+            const fontAvailable = [];
+    
+            for (const font of fontCheck.values()) {
+                if (document.fonts.check(`12px "${font}"`)) {
+                    fontAvailable.push(font);
+                }
+            }
+    
+            return fontAvailable
+        }
+    
+        getAvailableFonts().then((list) => setAvailableFonts(list));
+    }, []);
+
+    return (
+        <FontContext.Provider value={{availableFonts}}>
+            {children}
+        </FontContext.Provider>
+    )
+};
