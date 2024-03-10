119a3a17e870acbfae5b0dee0364469278286e72
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 16:32:22 +0100

    Adds isHTMLButtonElement typeguard

--- src/utils/typeguards.ts
+++ src/utils/typeguards.ts
@@ -1,13 +1,10 @@
 import { dataAsJSONEntryType } from "./types";
 
-// Type guard function
 export const isArrayOfDataAsJSONEntryType = (obj: any): obj is dataAsJSONEntryType[] => {
     if (!Array.isArray(obj)) return false;
-    // Check if every element in the array is of type dataAsJSONEntryType
     return obj.every((item) => isDataAsJSONEntryType(item));
 }
 
-// Helper function to check if an object is of type dataAsJSONEntryType
 export const isDataAsJSONEntryType = (obj: any): obj is dataAsJSONEntryType => {
     if (typeof obj !== "object" || obj === null) return false;
     for (const key in obj) {
@@ -16,4 +13,8 @@ export const isDataAsJSONEntryType = (obj: any): obj is dataAsJSONEntryType => {
         }
     }
     return false;
+}
+
+export const isHTMLButtonElement = (obj: any): obj is HTMLButtonElement => {
+    return obj instanceof HTMLButtonElement;
 }
