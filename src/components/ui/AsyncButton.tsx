08f15a4f1446710fabc34894b99174a063f6b2b8
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 13:36:37 +0100

    Adds AsyncButton ui component

---
+++ src/components/ui/AsyncButton.tsx
@@ -0,0 +1,38 @@
+import { PropsWithChildren } from "react"
+import ClipLoader from "./ClipLoader"
+
+type AsyncButtonType = PropsWithChildren<{
+    isLoading: boolean,
+    size: number,
+    color: string,
+}>
+
+const AsyncButton = ({
+    isLoading,
+    children,
+    size=20,
+    color="rgb(209,213,219)",
+    ...props
+}: AsyncButtonType) => {
+    return (
+        <button
+            {...props}
+            disabled={isLoading}
+        >
+            <p className={isLoading ? "hidden" : "visible"}>
+                {children}
+            </p>
+            {(isLoading && 
+                <div className="cliploader-centered">
+                    <ClipLoader
+                        color={color}
+                        loading={isLoading}
+                        size={size}
+                    />
+                </div>
+            )}
+        </button>
+    )
+}
+
+export default AsyncButton
