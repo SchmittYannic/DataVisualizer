f2a77b9743abdc655d318f9a945ffcf9d1903219
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-03-10 16:32:03 +0100

    Adds util functions

---
+++ src/utils/sortColumn.ts
@@ -0,0 +1,29 @@
+import { SortingFn } from "@tanstack/react-table"
+
+const sortColumn: SortingFn<any> = (prev, curr, columnId) => {
+    if (typeof prev.original[columnId] === "string" && curr.original[columnId] === "string") {
+        return sortCaseInsensitive(prev, curr, columnId)
+    } else if (typeof prev.original[columnId] === "string" && curr.original[columnId] === "number") {
+        return 1
+    } else if (typeof prev.original[columnId] === "number" && curr.original[columnId] === "string") {
+        return -1
+    } else if (prev.original[columnId] > curr.original[columnId]) {
+        return 1
+    } else if (prev.original[columnId] < curr.original[columnId]) {
+        return -1
+    } else {
+        return 0
+    }
+}
+
+const sortCaseInsensitive: SortingFn<any> = (prev, curr, columnId) => {
+    if (prev.original[columnId].toLowerCase() > curr.original[columnId].toLowerCase()) {
+        return 1;
+    } else if (prev.original[columnId].toLowerCase() < curr.original[columnId].toLowerCase()) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+export default sortColumn
